c1e07f0a534bada1cc56b589baa501ba76528f6c
tommy <163079978+tommyanhnguyen@users.noreply.github.com>
2025-09-25 16:53:50 +1000

    Update preprocess_and_train.py

--- preprocess_and_train.py
+++ preprocess_and_train.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import joblib
 
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
