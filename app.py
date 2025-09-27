b0b608a2503b492e0ddbf60f973a22003c3e5d5a
tommy <163079978+tommyanhnguyen@users.noreply.github.com>
2025-09-28 00:07:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import joblib
 import pandas as pd
-import numpy as np
+# import numpy as np
 import streamlit as st
 
 st.set_page_config(page_title="House Price Predictor", layout="centered")
